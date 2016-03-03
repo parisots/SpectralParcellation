db29422b32d07307f5bc9de45055775abf5ecb0f
Sarah Parisot <parisot.sarah@gmail.com>
2016-03-03 18:29:18 +0000

    Update Ncut_multi.m

--- Ncut_multi.m
+++ Ncut_multi.m
@@ -15,22 +15,6 @@ function [NcutDiscrete,NcutEigenvectors,Eigenvalues]=Ncut_multi(Wtot,C,d,Nclus)
 % % NcutEigenvectors: Eigenvectors of the 
 % % CorMat: Correlation Matrix of MatM
 
-% Copyright (C) Sarah Parisot, Imperial College London, 2015
-%
-%This program is free software: you can redistribute it and/or modify
-%it under the terms of the GNU General Public License as published by
-%the Free Software Foundation, either version 3 of the License, or
-%(at your option) any later version.
-%
-%This program is distributed in the hope that it will be useful,
-%but WITHOUT ANY WARRANTY; without even the implied warranty of
-%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%GNU General Public License for more details.
-%
-%You should have received a copy of the GNU General Public License
-%along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 %%
 
 n=size(Wtot,1);
